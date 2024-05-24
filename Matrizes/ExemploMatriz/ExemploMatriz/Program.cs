2d7ac2c6f7ef4607f4b7ef6ea6834e1c40c87dbe
EZEQUIEL TEIXEIRA SANTOS <57824564851@info.local>
2024-05-24 15:56:34 -0300

    feat: Exemplos Matriz

---
+++ Matrizes/ExemploMatriz/ExemploMatriz/Program.cs
@@ -0,0 +1,31 @@
+﻿int l, c;
+int[,] a;
+
+string[] s = Console.ReadLine().Split(' ');
+
+l = int.Parse(s[0]);
+c = int.Parse(s[1]);
+
+a = new int[l, c];
+
+// para receber os valores nos seus devidos lugares
+for (int i = 0; i < l; i++)
+{
+    string[] vet = Console.ReadLine().Split(' ');
+
+    for (int j = 0; j < c; j++)
+    {
+        a[i,j] = int.Parse(vet[j]);
+    }
+}
+
+Console.WriteLine(); // para pular uma linha
+
+for (int i = 0; i < l; i++)
+{
+    for (int j = 0; j < c; j++)
+    {
+        Console.Write(a[i,j] + " ");
+    }
+    Console.WriteLine();
+}
---
+++ Matrizes/ExemploMatriz_2/ExemploMatriz_2/Program.cs
@@ -0,0 +1,42 @@
+﻿int n;
+int[,] a;
+
+n = int.Parse(Console.ReadLine());
+
+a = new int[n, n];
+
+for (int i = 0; i < n; i++)
+{
+    string[] vet = Console.ReadLine().Split(' ');
+    for (int j = 0; j < n; j++)
+    {
+        a[i, j] = int.Parse(vet[j]);
+    }
+}
+
+Console.WriteLine(); // para pular uma linha
+
+Console.WriteLine("Diagonal Principal: ");
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write(a[i, i] + " ");
+}
+
+Console.WriteLine(); // para pular uma linha
+
+int cont = 0;
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (a[i, j] < 0)
+        {
+            cont++;
+        }
+    }
+}
+
+Console.WriteLine("Quantidade de negativos: " + cont);
+
