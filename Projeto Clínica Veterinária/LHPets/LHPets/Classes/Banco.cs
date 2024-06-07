70b9d63957975bbd472c08b4dad882df2b070fe4
EZEQUIEL TEIXEIRA SANTOS <57824564851@info.local>
2024-06-07 16:47:04 -0300

    feat: Projeto Clínica Veterinária

---
+++ Projeto Clínica Veterinária/LHPets/LHPets/Classes/Banco.cs
@@ -0,0 +1,50 @@
+﻿namespace LHPets.Classes
+{
+    public class Banco
+    {
+        private List<Cliente> lista = new List<Cliente>();
+
+        public List<Cliente> GetLista()
+        {
+            return lista;
+        }
+
+        public string GetListaString()
+        {
+            string enviar = "<!DOCTYPE html>\n<html>\n<head>\n<meta charset='utf-8'/>\n" +
+                            "<title>Cadastro de Cliente</title>\n" +
+                            "<style>table{border-spacing: 0; border-collapse: collapse;} th, td{border: 1px solid black; padding: 0 10px;} tbody tr:nth-child(even){background: #ccc}</style>" +
+                            "</head>\n<body>" +
+                            "<h1>Lista de Cliente</h1>" +
+                            "<table><thead><tr>" +
+                                "<th>CPF/CNPJ</th>" +
+                                "<th>Nome</th>" +
+                                "<th>Endereço</th>" +
+                                "<th>RG/IE</th>" +
+                                "<th>Tipo</th>" +
+                                "<th>Valor</th>" +
+                                "<th>Valor Imposto</th>" +
+                                "<th>Total</th>" +
+                            "</thead><tbody>";
+
+            foreach (Cliente cli in GetLista())
+            {
+                enviar += "<tr>" +
+                    $"<td>{cli.cpf_cpnj}</td>" +
+                    $"<td>{cli.nome}</td>" +
+                    $"<td>{cli.endereco}</td>" +
+                    $"<td>{cli.rg_ie}</td>" +
+                    $"<td>{cli.tipo}</td>" +
+                    $"<td>{cli.valor.ToString("C")}</td>" +
+                    $"<td>{cli.valor_imposto.ToString("C")}</td>" +
+                    $"<td>{cli.total.ToString("C")}</td>" +
+                    "</tr>";
+            }
+
+            enviar += "</tboby></table></body></html>";
+
+            return enviar;
+        }
+
+    }
+}
---
+++ Projeto Clínica Veterinária/LHPets/LHPets/Classes/Cliente.cs
@@ -0,0 +1,26 @@
+﻿namespace LHPets.Classes
+{
+    public class Cliente
+    {
+        public Cliente(string cpf_cpnj, string nome, string endereco, string rg_ie, char tipo, float valor, float valor_imposto, float total)
+        {
+            this.cpf_cpnj = cpf_cpnj;
+            this.nome = nome;
+            this.endereco = endereco;
+            this.rg_ie = rg_ie;
+            this.tipo = tipo;
+            this.valor = valor;
+            this.valor_imposto = valor_imposto;
+            this.total = total;
+        }
+
+        public string cpf_cpnj { get; set; }
+        public string nome { get; set; }
+        public string endereco { get; set; }
+        public string rg_ie { get; set; }
+        public char tipo { get; set; }
+        public float valor { get; set; }
+        public float valor_imposto { get; set; }
+        public float total { get; set; }
+    }
+}
